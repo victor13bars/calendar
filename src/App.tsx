00c5c369f474aba422643aa13d86dbe886145d20
victor13bars <victor_bars_bars95@mail.ru>
2021-10-15 18:17:28 +0300

    Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React, {FC} from 'react';
+import AppRouter from "./components/AppRouter";
 
 const App:FC = () => {
   return (
       <div>
-        WORK
+       <AppRouter/>
       </div>
   );
 };
